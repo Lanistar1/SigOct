588232d727627f70b6334ee22df324f010ebc2fb
Lanistar1 <emmanuelola92@gmail.com>
2022-10-26 13:52:02 +0100

    made changes across all pages

---
+++ SigmaPOS/Models/DashBoardModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SigmaPOS.Models
+{
+    public class DashBoardModel
+    {
+
+    }
+}
---
+++ SigmaPOS/ViewModels/WithdrawViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SigmaPOS.ViewModels
+{
+    public class WithdrawViewModel
+    {
+
+    }
+}
